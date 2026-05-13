1a8b81d72a8749bb42e15b168dea9c4706dc20bd
Pablo Fallas <pfallasro@gmail.com>
2026-05-13 11:37:48 +0200

    feat: German legal compliance (Impressum + Datenschutz + self-hosted fonts) (#16)

--- infra/states/pablofallas.name/main.tf
+++ infra/states/pablofallas.name/main.tf
@@ -142,9 +142,9 @@ resource "aws_cloudfront_response_headers_policy" "security_headers" {
       content_security_policy = join("; ", [
         "default-src 'self'",
         "script-src 'self' 'unsafe-inline' https://cloud.umami.is",
-        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+        "style-src 'self' 'unsafe-inline'",
         "img-src 'self' data: https:",
-        "font-src 'self' data: https://fonts.gstatic.com",
+        "font-src 'self' data:",
         "connect-src 'self' https://cloud.umami.is",
         "manifest-src 'self'",
         "worker-src 'self'",
